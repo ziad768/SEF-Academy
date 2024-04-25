545b8784723b77f830475563148d4cbf8990cb2d
Ziad Hamdi Saad <130312784+ziad768@users.noreply.github.com>
2024-04-25 02:19:15 +0200

    Update ModelLogin.jsx

--- src/Components/ModelLogin.jsx
+++ src/Components/ModelLogin.jsx
@@ -1,21 +1,14 @@
-import React from 'react'
-
 function ModelLogin() {
   return (
     <>
-      <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
-  Launch demo modal
-</button>
-<div className="modal fade" id="exampleModalCenter" tabIndex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="false">
-  <div className="modal-dialog modal-dialog-centered" role="document">
-    <div className="modal-content">
-      <div className="modal-header">
-        <h5 className="modal-title" id="exampleModalCenterTitle">Account Login</h5>
-        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
-      <div className="modal-body">
+      <div class="modal-body">
         Admin Account : dl-2851 <br/>
         Admin Password : 12345678
         <br/>
@@ -27,13 +20,14 @@ function ModelLogin() {
         Student Account : dl-51014 <br/>
         Student Password : 12345678
       </div>
-      <div className="modal-footer">
-        <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" className="btn btn-primary">Save changes</button>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
       </div>
     </div>
   </div>
 </div>
+
 </>
   )
 }
